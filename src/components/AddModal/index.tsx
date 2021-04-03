1be306818b649b146f3a620151183c14461afd85
Matheus Góes <matheusvmg@gmail.com>
2021-04-03 17:22:42 -0300

    create the add tool modal

--- src/components/AddModal/index.tsx
+++ src/components/AddModal/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Background } from "./styles";
+import { Background, Modal, StyledAddBtn } from "./styles";
 
 interface AddModalProperty {
   show: Function;
@@ -7,11 +7,23 @@ interface AddModalProperty {
 
 const AddModal: React.FC<AddModalProperty> = ({ show }) => {
   return (
-    <Background
-      onClick={() => {
-        show(false);
-      }}
-    ></Background>
+    <Background onClick={() => show(false)}>
+      <Modal onClick={(e) => e.stopPropagation()}>
+        <div id="header">
+          <span>+</span>
+          <h4>Add tool</h4>
+        </div>
+        <p className="labels">Tool Name</p>
+        <input type="text" />
+        <p className="labels">Tool Link</p>
+        <input type="text" />
+        <p className="labels">Tool description</p>
+        <input type="text" />
+        <p className="labels">Tags</p>
+        <input type="text" />
+        <StyledAddBtn type="button">Add tool</StyledAddBtn>
+      </Modal>
+    </Background>
   );
 };
 
--- src/components/AddModal/styles.ts
+++ src/components/AddModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import colors from "../../config/colors";
 
 export const Background = styled.div`
   position: fixed;
@@ -10,4 +11,85 @@ export const Background = styled.div`
   height: 100%;
   width: 100%;
   background: rgba(0, 0, 0, 0.8);
+`;
+
+export const Modal = styled.div`
+  width: 87vw;
+  height: auto;
+  border-radius: 5px;
+  background-color: ${colors.mostDarkesWhite};
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+
+  #header {
+    display: flex;
+    align-items: center;
+    margin-bottom: 15px;
+    span {
+      font-size: 26px;
+      color: #170c3a;
+      margin-right: 5px;
+      font-weight: 800;
+    }
+    h4 {
+      font-size: 22px;
+      color: #170c3a;
+      font-weight: 600;
+    }
+  }
+
+  .labels {
+    color: ${colors.lightInk};
+    font-size: 14px;
+
+    @media (min-width: 768px) {
+      margin: 0px 30px;
+    }
+  }
+
+  input {
+    height: 30px;
+    margin: 10px 0px;
+    padding: 20px;
+    border: 1px solid ${colors.darkestWhite};
+    background-color: ${colors.darkerWhite};
+    color: ${colors.ink};
+    font-size: 16px;
+
+    &:nth-child(7) {
+      padding: 0px 20px;
+      height: 80px;
+    }
+
+    @media (min-width: 768px) {
+      margin: 10px 30px;
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 60vw;
+  }
+  @media (min-width: 1024px) {
+    width: 50vw;
+  }
+`;
+
+export const StyledAddBtn = styled.button`
+  width: 100%;
+  height: 40px;
+  background-color: ${colors.red};
+  border: 0px;
+  outline: 0 none;
+  border-radius: 5px;
+  font-weight: 700;
+  color: ${colors.white};
+  cursor: pointer;
+  margin-top: 5px;
+
+  @media (min-width: 768px) {
+    width: 100px;
+    align-self: flex-end;
+    margin-right: 30px;
+  }
 `;
