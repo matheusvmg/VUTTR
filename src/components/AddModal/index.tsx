df2913f943560838bc89ec6b9dd57241b5a863d9
matheus.goes <matheus.goes@accurate.com.br>
2021-05-20 16:38:31 -0300

    remove the useEffect that is never used

--- src/components/AddModal/index.tsx
+++ src/components/AddModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Background, Modal, StyledAddBtn } from "./styles";
 import addTool from "../../services/addTool";
 
